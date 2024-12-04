e60e59a3019daee7b37db2c4b5fb959de3271f1f
Todd Musall <musallt@hurco.com>
2024-12-04 06:00:25 -0500

    day 2 part 1 kinda

---
+++ AOC-2024/Day_2/advent.ex
@@ -0,0 +1,39 @@
+defmodule Advent do
+  def doWork(fileName) do
+    fileName
+    |> File.read!()
+    |> String.split("\n", trim: true)
+    |> String.split(" ")
+    # myList |> Enum.chunk_every(2,1, :discard)
+    # s |> String.split(" ") |> Enum.map(fn x -> String.to_integer(x) end)
+    #  [7, 6, 4, 2, 1]
+    #|> Enum.map(fn entry ->
+    #    [left, right] = String.split(entry)
+    #    {String.to_integer(left), String.to_integer(right)}
+    #  end)
+    #|> Enum.unzip()
+    #|> findTotalDistance()
+    #|> findSimilarity()
+  end
+
+  # Part 1
+  defp findTotalDistance({leftList, rightList}) do
+    distance =
+      Enum.zip(Enum.sort(leftList), Enum.sort(rightList))
+      |> Enum.map(fn {l, r} -> abs(l-r) end)
+      |> Enum.sum()
+    IO.puts("Total Distance: #{distance}")
+    {leftList, rightList}
+  end
+
+  # Part 2
+  defp findSimilarity({leftList, rightList}) do
+    right_counts = rightList |> Enum.frequencies()
+
+    similarity =
+      Enum.reduce(leftList, 0, fn location_id, acc ->
+      acc + location_id * Map.get(right_counts, location_id, 0)
+      end)
+    IO.puts("Similarity: #{similarity}")
+  end
+end
